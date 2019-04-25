6fdcc872cf453623230c75c169c23813bc4bbfa4
niclasmattsson <33093224+niclasmattsson@users.noreply.github.com>
2019-04-25 17:11:24 +0200

    Save GHI and DHI in makesolar

--- src/makesolarera5.jl
+++ src/makesolarera5.jl
@@ -13,15 +13,11 @@ function makesolarera5(year=2018, land_cells_only=true)
     println("Creating HDF5 file:  $filename")
     h5open(filename, "w") do file 
         group = file["/"]
-        dataset_diffuse = d_create(group, "diffuse", datatype(Float32), dataspace(gridsize...,hours), "chunk", (gridsize...,5), "blosc", 3)
-        dataset_direct = d_create(group, "direct", datatype(Float32), dataspace(gridsize...,hours), "chunk", (gridsize...,5), "blosc", 3)
-        dataset_meandiffuse = d_create(group, "meandiffuse", datatype(Float32), dataspace(gridsize...), "chunk", gridsize, "blosc", 3)
-        dataset_meandirect = d_create(group, "meandirect", datatype(Float32), dataspace(gridsize...), "chunk", gridsize, "blosc", 3)
+        # create GHI and DHI variables (Global Horizontal Irradiance and Diffuse Horizontal Irradiance)
+        dataset_GHI = d_create(group, "GHI", datatype(Float32), dataspace(gridsize...,hours), "chunk", (gridsize...,5), "blosc", 3)
+        dataset_DHI = d_create(group, "DHI", datatype(Float32), dataspace(gridsize...,hours), "chunk", (gridsize...,5), "blosc", 3)
 
-        totaldiffuse = zeros(gridsize)
-        totaldirect = zeros(gridsize)
         hour = 1
-
         for month = 1:12, monthhalf = 1:2
             if monthhalf == 1
                 firstday, lastday = "01", "15"
@@ -35,21 +31,16 @@ function makesolarera5(year=2018, land_cells_only=true)
 
             println("\nReading solar diffuse and direct components from $erafile...")
             ncdataset = Dataset(erafile)
-            # ERA5 radiations are in J/m2/period, so for hourly data divide by 3600 to get W/m2
-            diffuse = replace(ncdataset["ssrd"][:,:,:], missing => 0.0) .* (land .> 0) ./ 3600
-            direct = replace(ncdataset["fdir"][:,:,:], missing => 0.0) .* (land .> 0) ./ 3600
+            # ERA5 radiations are in J/m2/period, so for hourly data divide by 3600*1000 to get kW/m2
+            GHI = replace(ncdataset["ssrd"][:,:,:], missing => 0.0) .* (land .> 0) ./ (3600*1000)
+            DHI = GHI - replace(ncdataset["fdir"][:,:,:], missing => 0.0) .* (land .> 0) ./ (3600*1000)
 
-            totaldiffuse = totaldiffuse + sum(diffuse, dims=3)
-            totaldirect = totaldirect + sum(direct, dims=3)
-            len = size(diffuse,3)
+            len = size(GHI,3)
             println("Writing to $filename...")
-            dataset_diffuse[:,:,hour:hour+len-1] = diffuse
-            dataset_direct[:,:,hour:hour+len-1] = direct
+            dataset_GHI[:,:,hour:hour+len-1] = GHI
+            dataset_DHI[:,:,hour:hour+len-1] = DHI
             hour += len
         end
-        println("\nWriting mean solar variables to $filename...")
-        dataset_meandiffuse[:,:] = totaldiffuse/hours
-        dataset_meandirect[:,:] = totaldirect/hours
     end
     nothing
 end
