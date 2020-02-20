d9211add8b4f00fe658d0cf3b4fe92920610b7fe
Niclas Mattsson <niclas.mattsson@gmail.com>
2020-02-20 17:11:16 +0100

    Simplify and speed up makedistances()

--- src/makedistances.jl
+++ src/makedistances.jl
@@ -13,10 +13,16 @@ function makedistances(gisregion; scenarioyear="ssp2_2050", res=0.01)
 
     geocenters, popcenters = regioncenters(regions, numreg, popdens, lonrange, latrange, res)
     println("\nCalculating distances between centers...")
-    distances = [greatcircledistance(popcenters[r1], popcenters[r2]) for r1 = 1:numreg, r2 = 1:numreg]
-    connected, connectedoffshore = connectedregions(regions, offshoreregions, numreg)
-    regionpop = [sum(pop.==r) for r = 1:numreg]
+    distances = [greatcircledistance(Tuple(popcenters[r1,[2,1]]), Tuple(popcenters[r2,[2,1]])) for r1 = 1:numreg, r2 = 1:numreg]
 
+    println("\nFinding neighboring regions for transmission connections...")
+    println("...onshore...")
+    connected = connectedregions(regions, numreg)
+    println("...offshore...")
+    connectedoffshore = connectedregions(offshoreregions, numreg)
+    # regionpop = [sum(pop.==r) for r = 1:numreg]
+
+    println("\nSaving results...")
     matopen(joinpath(outputfolder, "distances_$gisregion.mat"), "w") do file
         write(file, "distances", distances)
         write(file, "connected", connected)
@@ -24,8 +30,8 @@ function makedistances(gisregion; scenarioyear="ssp2_2050", res=0.01)
         write(file, "regionlist", string.(regionlist))
         # write(file, "population", pop)
         # write(file, "demand", demand)
-        write(file, "regioncenters_lat", [latlon[1] for latlon in popcenters])
-        write(file, "regioncenters_lon", [latlon[2] for latlon in popcenters])
+        write(file, "regioncenters_lon", popcenters[:,1])
+        write(file, "regioncenters_lat", popcenters[:,2])
     end
 
     # Also save the region file in Matlab format
@@ -36,56 +42,48 @@ end
 # returns great circle distance in km between points given as (lat,lon) tuples (in degrees).
 greatcircledistance(point1::Tuple, point2::Tuple) = haversine(point1, point2, 6371.0)
 
-# evaluate a vector at a float index usinglinear interpolation between elements
-function lerp(vect::AbstractArray, floatindex::Float64)
-    floatindex >= length(vect) && return vect[end]
-    f, index = modf(floatindex)
-    index = clamp(Int(index), 1, length(vect)-1)
-    return vect[index]*(1-f) + vect[index+1]*f
-end
-# lerp(vect::AbstractArray, floatindex::Float64) = vect[floor(Int, floatindex)]
-
 function regioncenters(regions, numreg, popdens, lonrange, latrange, res)
     println("Finding area- and population-weighted region centers...")
-    lons = (-180+res/2:res:180-res/2)         # longitude values (pixel center)
-    lats = (90-res/2:-res:-90+res/2)          # latitude values (pixel center)
-    geocenters = Vector{Tuple{Float64,Float64}}(undef, numreg)
-    popcenters = Vector{Tuple{Float64,Float64}}(undef, numreg)
-    updateprogress = Progress(numreg, 1)
-    for r = 1:numreg
-        regmask = (regions.==r)
-        latindex = sum(latrange'.*regmask)/sum(regmask)
-        lonindex = sum(lonrange.*regmask)/sum(regmask)
-        geocenters[r] = lerp(lats, latindex), lerp(lons, lonindex)
-
-        regpopdens = popdens.*regmask
-        latindex = sum(latrange'.*regpopdens)/sum(regpopdens)
-        lonindex = sum(lonrange.*regpopdens)/sum(regpopdens)
-        popcenters[r] = lerp(lats, latindex), lerp(lons, lonindex)
-        next!(updateprogress)
+    lons = (-180+res/2:res:180-res/2)[lonrange]         # longitude values (pixel center)
+    lats = (90-res/2:-res:-90+res/2)[latrange]          # latitude values (pixel center)
+    rows, cols = size(regions)
+    geocenters = zeros(numreg,2)
+    popcenters = zeros(numreg,2)
+    counts = zeros(Int, numreg)
+    popdenssum = zeros(numreg)
+    for c = 1:cols
+        lat = lats[c]
+        for r = 1:rows
+            reg = regions[r,c]
+            (reg == 0 || reg == NOREGION) && continue
+            lon = lons[r]
+            geocenters[reg,:] += [lon, lat]
+            popcenters[reg,:] += popdens[r,c] .* [lon, lat]
+            counts[reg] += 1
+            popdenssum[reg] += popdens[r,c]
+        end
     end
+    geocenters ./= counts
+    popcenters ./= popdenssum    
     return geocenters, popcenters
 end
 
-function getneighbors(regions, r, numreg)
-    regmask = (regions.==r)
-    regmask_expanded = (imfilter(regmask, diskfilterkernel(2)) .> 0)    # expand mask with 1 pixel
-    neighbormask = regmask_expanded .& .!regmask
-    neighbors = unique(regions[neighbormask])
-    return neighbors[(neighbors.>0) .& (neighbors.!=NOREGION) .& (neighbors.<=numreg) .& (neighbors.!=r)]
-end
-
-function connectedregions(regions, offshoreregions, numreg)
-    println("\nDetermining onshore and offshore region connections...")
-    connected, connectedoffshore = zeros(Bool, numreg, numreg), zeros(Bool, numreg, numreg)
-    updateprogress = Progress(numreg, 1)
-    for r = 1:numreg     
-        neighbors = getneighbors(regions, r, numreg)
-        connected[r, neighbors] .= true
-
-        neighborsoffshore = getneighbors(offshoreregions, r, numreg)
-        connectedoffshore[r, neighborsoffshore] .= .!connected[r, neighborsoffshore]
-        next!(updateprogress)
+function connectedregions(regions, numreg)
+    connected = zeros(Bool, numreg, numreg)
+    rows, cols = size(regions)
+    for c = 1:cols
+        for r = 1:rows
+            reg = regions[r,c]
+            (reg == 0 || reg == NOREGION) && continue
+            for adjc = max(1, c-1):min(cols, c+1), adjr = max(1, r-1):min(rows, r+1)
+                reg2 = regions[adjr,adjc]
+                (reg2 == 0 || reg2 == NOREGION) && continue
+                connected[reg,reg2] = true
+            end
+        end
     end
-    return connected, connectedoffshore
+    for c = 1:numreg, r = 1:numreg
+        connected[r,c] = connected[r,c] || connected[c,r]
+    end 
+    return connected
 end
