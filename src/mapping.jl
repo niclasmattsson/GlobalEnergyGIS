bd3d645fd43daa48cc1fd57a77afb97e46b6eaf8
niclasmattsson <33093224+niclasmattsson@users.noreply.github.com>
2024-12-12 14:33:19 +0100

    Add VGR data for Pandu + map extents

--- src/mapping.jl
+++ src/mapping.jl
@@ -15,7 +15,7 @@ function createmap(gisregion, regions, regionlist, lons, lats, colors, source, d
 
     xmin, xmax = extrema(lons)
     ymin, ymax = extrema(lats)
-    lims = ((floor(xmin)-0.1, ceil(xmax)+8.0), (floor(ymin)-0.1, ceil(ymax)+0.1))
+    lims = ((floor(xmin)-0.1, ceil(xmax)+0.5), (floor(ymin)-0.1, ceil(ymax)+0.1))
     aspect_ratio = (ymax - ymin) / (xmax - xmin)
     pngwidth = max(1200, round(Int, resolutionscale*1.02*size(regions,1))) # allow for margins (+1% on both sides)
     pngsize = pngwidth, round(Int, pngwidth * aspect_ratio)     # use aspect ratio after projection transformation
@@ -401,6 +401,72 @@ function ehub500()
     return buses
 end
 
+function vgrdata(num_nodes=104; plotmasks=true, exclude_croplands_PV=false)
+    plotmasks && createmaps("vgr$num_nodes", lines=false, labels=true)
+    landclasses = exclude_croplands_PV ? [0,1,2,3,4,5,8,12] : [0,1,2,3,4,5,8]
+    # landclasses = exclude_croplands_PV ? [0,1,2,3,4,5,8,12,14] : [0,1,2,3,4,5,8]
+    GISsolar(gisregion="vgr$num_nodes"; era_year=2019, plotmasks=plotmasks, grid_everywhere=true, exclude_landtypes=landclasses,
+                pvclasses_min=[0.08], pvclasses_max=[1.0], cspclasses_min=[0.10], cspclasses_max=[1.0])
+    GISwind(gisregion="vgr$num_nodes"; era_year=2019, plotmasks=plotmasks, grid_everywhere=true,
+                onshoreclasses_min=[6], onshoreclasses_max=[99], offshoreclasses_min=[7], offshoreclasses_max=[99])
+end
+
+function create_vgr_data(num_nodes=104)
+    # saveregions("VGR", ["VGR"  GADM(["Sweden"], "Västra Götaland")])
+    # vgr_munic = subregions(GADM, "Sweden", "Västra Götaland")
+    # saveregions("VGR_munic", [vgr_munic  [GADM(["Sweden", "Västra Götaland"], vv) for vv in vgr_munic]])
+
+    # rasterize_vgr(num_nodes) # for Pandu's Voronoi, not used anymore
+    geojson = in_datafolder("vgr", "voronoi_cells_$(num_nodes)nodes.geojson")
+    df = GDF.read(in_datafolder("vgr", "voronoi_cells_$(num_nodes)nodes.geojson"))
+    buses = CSV.read(in_datafolder("Bus_and_line_data_EHUB400_future_data_v1_6 - buses.csv"), DataFrame)
+    # buses = buses[.!(buses.type .== "TRAFO" .&& buses[!, "Voltage [kV]"] .> 220), :]
+    unique!(buses, ["x-coordinate", "y-coordinate"])    # assumes voltages are in ascending order for each location
+    xy = Matrix(buses[:, ["x-coordinate", "y-coordinate"]])
+    # @show extrema(xy[:,1])
+    # @show extrema(xy[:,2])
+    bbox = (4.5, 31.6, 54.8, 71.2) .+ (-1, 1, -1, 1)   # <-- tight,  [52.90 2.24; 72.84 33.24] with 6% padding
+
+    cc = first.(buses.bidding_zone, 2)  # country code
+
+    country = "SE"  # for in ["SE", "NO", "DK", "FI"]
+
+    tri = triangulate(xy[cc .== country, :]')
+    vorn = voronoi(tri)
+
+    # fig = Figure()
+    # ax = GeoMakie.Axis(fig[1, 1], title="(f): Voronoi tessellation", width=400, height=400)
+    # voronoiplot!(ax, vorn, show_generators=false)
+    # display(fig)
+
+    n = num_polygons(vorn)
+    poly = [Point.(DelaunayTriangulation.get_polygon_coordinates(vorn, i, bbox)) |> GeoMakie.Polygon for i = 1:n]
+    # poly = [Point.(DelaunayTriangulation.get_polygon_coordinates(vorn, i, bbox)) |> GeoMakie.Polygon for i = 1:n] |> GeoMakie.MultiPolygon
+
+    df = DataFrame(geometry=poly, FID=1:n, bus_id=buses.bus_id)
+
+    
+
+    vgr = readraster(in_datafolder("vgr$num_nodes.tif"))
+    regions, offshoreregions, regionlist, lonrange, latrange = loadregions("VGR")
+
+    res = 0.01
+    res2 = res/2
+    lons = (-180+res2:res:180-res2)[lonrange]         # longitude values (pixel center)
+    lats = (90-res2:-res:-90+res2)[latrange]          # latitude values (pixel center)
+    bbox = round.([lats[end]-res2 lons[1]-res2; lats[1]+res2 lons[end]+res2], digits=2)
+    @show bbox
+
+    vv = @view vgr[lonrange, latrange]
+    noreg = (regions .== NOREGION .|| offshoreregions .== NOREGION)
+    vv[noreg] .= NOREGION
+
+    saveregions("vgr$num_nodes", df.node_id, vgr; autocrop=false, bbox)
+    makedistances("vgr$num_nodes")
+end
+
+intersectedarea(p1,p2) = intersects(p1,p2) ? area(intersection(p1,p2)) : 0.0
+
 function ginfo(infile)
     gdalinfo_path() do gdalinfo
         run(`$gdalinfo $infile`)
@@ -425,6 +491,18 @@ function rasterize_ehub500()
 
     nothing
 end
+
+## Can't use Pandu's Voronoi data, maps incompatible with my GADM regions and coastlines
+# function rasterize_vgr(num_nodes=104)
+#     println("\nRasterizing vgr geojson...")
+#     geojson = in_datafolder("vgr", "voronoi_cells_$(num_nodes)nodes.geojson")
+#     outfile = in_datafolder("vgr$num_nodes.tif")
+#     options = "-a vor_id -ot Int32 -tr 0.01 0.01 -te -180 -90 180 90 -co COMPRESS=LZW"
+#     # options = "-a UID -ot Int32 -tr 0.02 0.02 -te -180 -90 180 90 -co COMPRESS=LZW"
+#     @time rasterize(geojson, outfile, split(options, ' '))
+
+#     nothing
+# end
 
 function inpolys(point, polygons)
     for (i, poly) in enumerate(polygons)
